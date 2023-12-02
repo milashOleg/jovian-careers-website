e3483519029d6fb7390f0a504fb4d8e71f7e23f6
milashOleg <omilash@mail.ru>
2023-12-02 15:39:51 +0000

    added API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -35,6 +35,10 @@ def hello_world():
                          jobs=JOBS,
                         company_name='Jovian')
 
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
